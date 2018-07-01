596dd748f31481676f2da3d07e115afbf344ea50
Jesse McReady <jessemcready@gmail.com>
2018-07-01 02:09:13 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,5 +1,5 @@
 def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
-  arr.sort_by { |word| esperanto_alphabet.index(word) }
+  arr.sort_by { |word| esperanto_alphabet.index(word) }.join
 end
