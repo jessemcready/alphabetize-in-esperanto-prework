c85b3a2bd536f0bbe99173b995226561943dfb52
Jesse McReady <jessemcready@gmail.com>
2018-07-01 02:16:00 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ def alphabetize(arr)
   # code here
   esperanto_alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "abcdefghijklmnopqrstuvwxyz"
-  arr.sort_by { |word| esperanto_alphabet}
+  arr.sort_by { |word| word.tr(esperanto_alphabet, ascii) }
 end
